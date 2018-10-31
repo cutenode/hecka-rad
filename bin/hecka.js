992ba745ba6e9b78b196b5eeddbfba1284320de0
Tierney Cyren <hello@bnb.im>
2018-10-31 14:17:04 -0400

    Minimum working code

---
+++ bin/hecka.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var chalk = require("chalk")
+
+console.log(chalk.red("╔══════════════════════════════════════════════════════════════════════╗"))
+console.log(chalk.red("║                              " + chalk.yellow("Hecka Rad") + "                               ║"))
+console.log(chalk.red("║            " + chalk.yellow("This message brought to you by Dan Hedgecock") + "              ║"))
+console.log(chalk.red("╚══════════════════════════════════════════════════════════════════════╝"))
